8cb07db9e63ecb2a897630d4653c9f19dd763857
lbodlev888 <lbodlev888@gmail.com>
2025-03-13 15:24:21 +0200

    Added gitignore file

--- server.js
+++ server.js
@@ -52,12 +52,8 @@ app.post('/conectare', function(req, res) {
 
     con.query(`SELECT id_user FROM users WHERE email='${email}' and password='${hash}'`, function(err, result) {
         if(err) throw err;
-        if(Object.keys(result).length === 0) {
-            res.redirect(`/conectare?failed`);
-        }
-        else {
-            res.redirect(`/index.html?connected`);
-        }
+        if(Object.keys(result).length === 0) res.redirect(`/conectare?failed`);
+        else res.redirect(`/index.html?connected`);
     });
 });
 
