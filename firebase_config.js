57d9866b163e93d37890f1ac1d35012d8e81f740
lbodlev888 <lbodlev888@gmail.com>
2025-04-27 16:45:21 +0300

    added firebase config file

---
+++ firebase_config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0wVt3hOV4_bBVL9pKSrPLX_6WRPDbjno",
+  authDomain: "helo-1c0da.firebaseapp.com",
+  projectId: "helo-1c0da",
+  storageBucket: "helo-1c0da.firebasestorage.app",
+  messagingSenderId: "796541270047",
+  appId: "1:796541270047:web:806cab0ccfc95fbafe7d2a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
