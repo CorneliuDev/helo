1aedfa9857aec0364548905cdc5f94c9555084ea
lbodlev888 <lbodlev888@gmail.com>
2025-06-18 12:24:18 +0300

    full project

---
+++ fill_meili.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv');
+const { initializeFirebaseApp, getDataWithPagination } = require('./firebase');
+const meili = require('meilisearch');
+
+dotenv.config();
+
+const meiliClient = new meili.MeiliSearch({
+    host: process.env.MEILI_API_HOST,
+    apiKey: process.env.MEILI_API_KEY
+});
+
+initializeFirebaseApp();
+
+async function main()
+{
+    const products = await getDataWithPagination("products", {}, 0, 1000, "id_product");
+    meiliClient.index('mobileProducts').addDocuments(products);
+    console.log('123');
+}
+
+main();
